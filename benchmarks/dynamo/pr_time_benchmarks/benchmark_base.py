1f6f6820a858be550c0170e5e82dc869e3daf766
Laith Sakka <lsakka@meta.com>
2024-09-24 14:02:53 -0700

    enable failing diffs on regression
    ghstack-source-id: b7b7fe0e1c7314c84b09a0a3020a9557956cf16a
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/136551

--- benchmarks/dynamo/pr_time_benchmarks/benchmark_base.py
+++ benchmarks/dynamo/pr_time_benchmarks/benchmark_base.py
@@ -1,11 +1,14 @@
 import csv
 import gc
+import json
+import os
 from abc import ABC, abstractmethod
 
 from fbscribelogger import make_scribe_logger
 
 import torch._C._instruction_counter as i_counter
 import torch._dynamo.config as config
+import torch._logging.scribe as scribe
 from torch._dynamo.utils import CompileTimeInstructionCounter
 
 
@@ -52,14 +55,27 @@ struct TorchBenchmarkCompileTimeLogEntry {
 """,  # noqa: B950
 )
 
+# This is enabled only for OSS runs, we always run on the same machine, when running locally
+# expected values are different so by default this is not enabled.
+compare_results_with_expected = os.environ.get("PR_TIME_BENCHMARKS_TEST", "0") == "1"
+
 
 class BenchmarkBase(ABC):
-    # measure total number of instruction spent in _work.
+    # Measure total number of instruction spent in _work.
+    # Garbage collection is NOT disabled during _work().
     _enable_instruction_count = False
 
-    # measure total number of instruction spent in convert_frame.compile_inner
-    # TODO is there other parts we need to add ?
+    # Measure total number of instruction spent in convert_frame.compile_inner
+    # Garbage collection is disabled during _work() to avoid noise.
     _enable_compile_time_instruction_count = False
+
+    # A pair of (expected, noise_margin) to compare with the actual instruction count.
+    # Margin is a percentage that represent acceptable noise margin.
+    # For example(100, 0.1(10%)) means that the actual instruction count should be between 90 and 110.
+
+    # The same variable is used for both instruction_count and compile_time_instruction_count
+    # because we do not allow both of them to be enabled at the same time now since they both log to the same scuba field.
+    _expected_instruction_count_result = None
 
     # number of iterations used to run when collecting instruction_count or compile_time_instruction_count.
     _num_iterations = 5
@@ -68,12 +84,19 @@ class BenchmarkBase(ABC):
         self._num_iterations = value
         return self
 
-    def enable_instruction_count(self):
+    def enable_instruction_count(self, expected=None, noise_margin=None):
         self._enable_instruction_count = True
+        if expected is not None:
+            assert noise_margin is not None
+            self._expected_instruction_count_result = (expected, noise_margin)
+
         return self
 
-    def enable_compile_time_instruction_count(self):
+    def enable_compile_time_instruction_count(self, expected=None, noise_margin=None):
         self._enable_compile_time_instruction_count = True
+        if expected is not None:
+            assert noise_margin is not None
+            self._expected_instruction_count_result = (expected, noise_margin)
         return self
 
     def name(self):
@@ -93,6 +116,43 @@ class BenchmarkBase(ABC):
     def _prepare_once(self):  # noqa: B027
         pass
 
+    def _verify_instruction_count(self, result):
+        if not compare_results_with_expected:
+            return
+
+        def log(event_name):
+            scribe.open_source_signpost(
+                subsystem="pr_time_benchmarks",
+                name=event_name,
+                parameters=json.dumps(self.name()),
+            )
+
+        if self._expected_instruction_count_result is None:
+            log("instruction_count_disabled")
+            return
+
+        (expected, noise_margin) = self._expected_instruction_count_result
+        low = expected - expected * noise_margin
+        high = expected + expected * noise_margin
+
+        if result > high:
+            print(
+                f"**REGRESSION** benchmark {self.name()} failed, \
+actual instruction count {result} is higher than expected {expected} with noise margin {noise_margin} \
+if this is an expected regression, please update the expected instruction count in the benchmark",
+            )
+
+            log("instruction_count_test_fail_regression")
+
+        if result < low:
+            print(
+                f"**WIN** benchmark {self.name()} failed, \
+actual instruction count {result} is lower than expected {expected} with noise margin {noise_margin} \
+please update the expected instruction count in the benchmark",
+            )
+            # if the test is by passed
+            log("instruction_count_test_fail_win")
+
     def _count_instructions(self):
         print(f"collecting instruction count for {self.name()}")
         results = []
@@ -101,7 +161,9 @@ class BenchmarkBase(ABC):
             id = i_counter.start()
             self._work()
             count = i_counter.end(id)
+
             print(f"instruction count for iteration {i} is {count}")
+            self._verify_instruction_count(count)
             results.append(count)
         return min(results)
 
@@ -127,6 +189,7 @@ class BenchmarkBase(ABC):
                     )
                 print(f"compile time instruction count for iteration {i} is {count}")
                 results.append(count)
+                self._verify_instruction_count(count)
 
             config.record_compile_time_instruction_count = False
             return min(results)
