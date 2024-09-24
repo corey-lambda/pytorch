f02713d073a10acc296ad3a27fd030b623f3e0e6
Jesse Cai <jcjessecai@gmail.com>
2024-09-24 14:51:07 -0700

    [sparse][semi-structured] Add float8 dtype support to 24 sparsity
    Summary:
    
    This PR adds float8 support to cuSPARSELt and `to_sparse_semi_structured`.
    
    Support for this op happens through `torch._scaled_mm`.
    
    It also turns on cuSPARSELt by default and adds CUSPARSELT_MAX_ID to the
    backend.
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    ghstack-source-id: f1d68cf7a8bbeeb012c9f585508fd7a581af7dc9
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/136397

--- test/test_sparse_semi_structured.py
+++ test/test_sparse_semi_structured.py
@@ -1026,6 +1026,18 @@ class TestSparseSemiStructuredCUTLASS(TestCase):
 CUSPARSELT_NUM_ALG_IDS = 4
 CUSPARSELT_MIXED_DTYPE_SUPPORT = [torch.float16, torch.bfloat16, torch.int32]
 
+def to_float8(x, dtype=torch.float8_e4m3fn):
+    finfo = torch.finfo(dtype)
+    # Calculate the scale as dtype max divided by absmax
+    scale = finfo.max / x.abs().max().clamp(min=1e-12)
+    # scale and clamp the tensor to bring it to
+    # the representative range of float8 data type
+    # (as default cast is unsaturated)
+    x_scl_sat = (x * scale).clamp(min=finfo.min, max=finfo.max)
+    # Return both float8 data and the inverse scale (as float),
+    # as both required as inputs to torch._scaled_mm
+    return x_scl_sat.to(dtype), scale.float().reciprocal()
+
 
 class TestSparseSemiStructuredCUSPARSELT(TestCase):
     """
@@ -1034,8 +1046,31 @@ class TestSparseSemiStructuredCUSPARSELT(TestCase):
         torch._cslt_sparse_mm
     """
     def setUp(self):
+        SparseSemiStructuredTensor._FORCE_CUTLASS = False
         if "cusparselt" not in SEMI_STRUCTURED_SUPPORTED_BACKENDS:
             self.skipTest('cuSPARSELt not enabled')
+
+    @parametrize("out_dtype", [torch.float16, torch.bfloat16, torch.float32])
+    @parametrize("dense_input_shape", [(256, 128)])
+    def test_sparse_semi_structured_scaled_mm(
+        self, dense_input_shape, device, out_dtype
+    ):
+        print()
+        A = rand_sparse_semi_structured_mask(256, 128, dtype=torch.float16)
+        B = torch.rand(dense_input_shape, device=device).to(torch.float16).t()
+
+        A_fp8, A_scale = to_float8(A)
+        B_fp8, B_scale = to_float8(B)
+
+        A_fp8_sparse = to_sparse_semi_structured(A_fp8)
+
+        dense_result = torch._scaled_mm(
+            A_fp8, B_fp8, scale_a=A_scale, scale_b=B_scale, out_dtype=out_dtype
+        )
+        sparse_result = torch._scaled_mm(
+            A_fp8_sparse, B_fp8, scale_a=A_scale, scale_b=B_scale, out_dtype=out_dtype
+        )
+        torch.testing.assert_close(dense_result, sparse_result, rtol=7e-2, atol=7e-2)
 
     @parametrize("out_dtype", CUSPARSELT_MIXED_DTYPE_SUPPORT)
     @parametrize("dense_input_shape", [(128, 128)])
@@ -1082,17 +1117,14 @@ class TestSparseSemiStructuredCUSPARSELT(TestCase):
 
         torch.testing.assert_close(sparse_result, dense_result, rtol=1e-3, atol=1e-3)
 
-    @parametrize("alg_id", range(CUSPARSELT_NUM_ALG_IDS))
     @inference_dtypes
-    def test_cslt_sparse_mm_alg_id(self, device, dtype, alg_id):
-        # alg_id=3 not supported for float32 dtype
-        if dtype == torch.float32 and alg_id == 3:
-            return
+    def test_cslt_sparse_mm_alg_id(self, device, dtype):
         A = rand_sparse_semi_structured_mask(128, 128, dtype=dtype)
         A_compressed = torch._cslt_compress(A)
         B = torch.ones((128, 128), device=device).to(dtype)
 
         A_compressed = torch._cslt_compress(A)
+        alg_id = torch._cslt_sparse_mm_search(A_compressed, B.t())
         sparse_result = torch._cslt_sparse_mm(A_compressed, B.t(), alg_id=alg_id)
 
         dense_result = torch.mm(A.to(torch.float32), B.to(torch.float32))
@@ -1102,17 +1134,13 @@ class TestSparseSemiStructuredCUSPARSELT(TestCase):
 
     @inference_dtypes
     def test_cslt_sparse_mm_search(self, device, dtype):
-        A = rand_sparse_semi_structured_mask(128, 128, dtype=dtype)
+        A = rand_sparse_semi_structured_mask(256, 128, dtype=dtype)
         A_compressed = torch._cslt_compress(A)
         B = torch.ones((128, 128), device=device).to(dtype)
 
         A_compressed = torch._cslt_compress(A)
         alg_id = torch._cslt_sparse_mm_search(A_compressed, B.t())
-        # for cuSPARSELt v0.4.0 there is a bug where although there are 5 alg_ids, we run into an error
-        # when setting using the last one (4)
-        # in cuSPARSELt v0.5.0 there are only 4 alg_ids total, so we should remove the +1 here when we update.
-        # TODO Move this into the cuSPARSELt backendk
-        assert alg_id in range(CUSPARSELT_NUM_ALG_IDS + 1)
+        assert alg_id in range(torch.backends.cusparselt.get_max_alg_id())
 
     def test_cusparselt_backend(self):
         version = _get_torch_cuda_version()
